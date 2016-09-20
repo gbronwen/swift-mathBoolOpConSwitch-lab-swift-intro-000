9aed25835e81be115f662ce8299b48dffa119910
Goneril <goneril@Gs-MacBook-Pro.local>
2016-09-20 13:05:10 -0400

    math bool con

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -8,6 +8,8 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
@@ -16,6 +18,66 @@ class ViewController: UIViewController {
     
     
    // Implement your functions here
+    
+   func averageIsAbove75(_ farg:Double,_ sarg:Double,_ targ:Double) ->Bool{
+        let total = farg + sarg + targ
+        
+        let aves = total / Double(3)
+        
+        switch aves > 75.0{
+        case true:
+            return true
+        default:
+            return false
+        }
+    }
 
 
+func passwordCombo(username:String, password:Int) -> String{
+    
+    switch username {
+        case "Jerry":
+            switch password % 3 == 0{
+            case true:
+                return "Welcome!"
+            default:
+                return "Access Denied"
+        }
+        case "Elaine":
+            switch password % 3 == 0{
+            case true:
+                return "Welcome!"
+            default:
+                return "Access Denied"
+        }
+        case "Michael":
+            switch password % 3 == 0{
+            case true:
+                return "Welcome!"
+            default:
+                return "Access Denied"
+        }
+        default:
+            return "Access Denied"
+        
+    }
+    
+}
+
+func describe(emoji:String) ->String{
+    switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case  "🍕":
+            return "Pizza"
+        case  "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+    }
+}
 }
